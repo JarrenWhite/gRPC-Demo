2b14e42ae05f842c8c2048ebb40f1a2bcbb9bb9e
Phill <phillrucci@hotmail.co.uk>
2026-01-30 17:28:42 +0000

    Create secure server version.

--- server/server.py
+++ server/server.py
@@ -20,3 +20,30 @@ def serve():
 
     print("gRPC server running on port 50051")
     server.wait_for_termination()
+
+
+def serve_secure():
+    with open("server.key", "rb") as f:
+        private_key = f.read()
+
+    with open("server.crt", "rb") as f:
+        certificate_chain = f.read()
+
+    server_credentials = grpc.ssl_server_credentials(
+        [(private_key, certificate_chain)]
+    )
+
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10)
+    )
+
+    demo_pb2_grpc.add_DemoServiceServicer_to_server(
+        DemoService(),
+        server
+    )
+
+    server.add_secure_port("[::]:50051", server_credentials)
+    server.start()
+
+    print("Secure gRPC server running on port 50051")
+    server.wait_for_termination()
