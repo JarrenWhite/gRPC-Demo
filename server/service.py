39ce619330f4dd8d4ba4ed84e2c8b84f111e6e15
Phill <phillrucci@hotmail.co.uk>
2026-02-02 18:12:34 +0000

    Adjust API implementation to return correct result.

--- server/service.py
+++ server/service.py
@@ -3,11 +3,11 @@ import definitions.demo_pb2_grpc as demo_pb2_grpc
 
 class DemoService(demo_pb2_grpc.DemoServiceServicer):
 
-    def DemoService(self, request, context):
+    def IsTrue(self, request, context):
         value = request.value
 
         is_positive = value >= 0
 
-        return demo_pb2.IsTrue(
+        return demo_pb2.DemoResponse(
             is_true = is_positive
         )
