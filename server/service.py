3f09f2d9e38f46a580cf4ce33eb4f90cfe49bf93
Phill <phillrucci@hotmail.co.uk>
2026-02-04 19:14:44 +0000

    Specify new API service

--- server/service.py
+++ server/service.py
@@ -11,3 +11,15 @@ class DemoService(demo_pb2_grpc.DemoServiceServicer):
         return demo_pb2.DemoResponse(
             is_true = is_positive
         )
+
+class MyService(demo_pb2_grpc.MyServiceServicer):
+
+    def MyServiceApi(self, request, context):
+        value_one = request.value_one
+        value_two = request.value_two
+
+        return demo_pb2.MyServiceResponse(
+            sum = value_one + value_two
+            product = value_one * value_two
+            difference = value_one - value_two
+        )
