372fd502c135a755b7648060a3cbc2b2c52dd0e5
Phill <phillrucci@hotmail.co.uk>
2026-02-07 11:01:19 +0000

    Create and test python caller for the new fake api

--- caller/call_api.py
+++ caller/call_api.py
@@ -2,6 +2,8 @@ import grpc
 import definitions.demo_pb2 as demo_pb2
 import definitions.demo_pb2_grpc as demo_pb2_grpc
 
+from typing import Tuple
+
 
 def call_is_true(value: int) -> bool:
     channel_credentials = grpc.local_channel_credentials()
@@ -16,5 +18,26 @@ def call_is_true(value: int) -> bool:
         return response.is_true
 
 
+def call_my_service_api(value_one, value_two) -> Tuple[int, int, int]:
+    channel_credentials = grpc.local_channel_credentials()
+
+    with grpc.secure_channel("localhost:50051", channel_credentials) as channel:
+        stub = demo_pb2_grpc.MyServiceStub(channel)
+
+        request = demo_pb2.MyServiceRequest(
+            value_one=value_one,
+            value_two=value_two
+        )
+
+        response = stub.MyServiceApi(request)
+
+        return response.sum, response.product, response.difference
+
+
 if __name__ == "__main__":
     print(call_is_true(5))
+
+    api_sum, api_product, api_difference = call_my_service_api(3, 5)
+    print(api_sum)
+    print(api_product)
+    print(api_difference)
